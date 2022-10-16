2442bdbbcafc9ba46180dabc2071b385db3fc922
Russell Warner <rewarner@russellbits.com>
2022-10-16 14:23:44 -0400

    Updated config file with preprocessor.

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,27 @@
+/**
+NOTE:
+The dev server needs a restart when this file is changes
+*/
+
+/**
+TODO:
+Need to add a svelte preprocessor for Sass
+
+Don't forget to run first:
+npm i -D svelte-preprocess sass
+*/
+
 import adapter from '@sveltejs/adapter-auto';
+// import sveltePreprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	//preprocess: [
+    //sveltePreprocess(),
+  //],
 };
 
 export default config;
