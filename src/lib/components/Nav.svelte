e4dff54e74e137746c982a1303e76d18b8848b27
Russell Warner <rewarner@russellbits.com>
2022-10-18 22:07:08 +1100

    Broke out more components from old design.

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,26 @@
+<!-- Nav.svelte -->
+<nav id="menu">
+  <ul>
+    <li>
+      <a class="scroll" href="#developer">Developer</a>
+    </li>
+    <li>
+      <a class="scroll" href="#designer">Designer</a>
+    </li>
+    <li>
+      <a class="scroll" href="#writer">Writer</a>
+    </li>
+    <li>
+      <a class="scroll" href="#contact">Contact</a>
+    </li>
+  </ul>
+</nav>
+
+<style>
+nav#menu {
+	position: absolute;
+	top: 620px;
+	color: white;
+	height: 42px;
+}
+</style>
---
+++ src/lib/components/SocialLinks.svelte
@@ -0,0 +1,6 @@
+<ul id="socialbits">
+	<li><a href="http://pinterest.com/belovedleader"><i class="glyph-pinterest glyph-white"></i></a></li>
+	<li><a href="http://github.com/russellbits"><i class="glyph-github glyph-white"></i></a></li>
+	<li><a href="http://instagram.com/belovedleader"><i class="glyph-instagram glyph-white"></i></a></li>
+	<li><a href="http://twitter.com/belovedleader"><i class="glyph-twitter glyph-white"></i></a></li>
+</ul>
